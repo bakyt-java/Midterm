be0a4412a94a60136ec7df4976695a7e1619266b
Hello World app <bakyt.ikramov@iaau.edu.kg>
2022-04-10 05:42:37 +0600

    Initial commit

---
+++ src/test/java/bakyt/ikramov/com/Midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package bakyt.ikramov.com.Midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
